20380fa9fbb6c67cc08540d0375a7c235aa3d906
alvarto1296 <alvarto1296@gmail.com>
2024-11-03 21:11:22 -0400

    [fix] create token

--- src/main/java/com/labanda/streaming/configurations/JwtUtils.java
+++ src/main/java/com/labanda/streaming/configurations/JwtUtils.java
@@ -5,17 +5,21 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.function.Function;
 
+import javax.crypto.SecretKey;
+
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
 
 @Component
 public class JwtUtils {
 
-    private final String SECRET_KEY = "examportal"; // Asegúrate de mantener esta clave segura
+    // Usa una clave secreta más segura y adecuada
+    private final SecretKey SECRET_KEY = Keys.hmacShaKeyFor("examportalexamportalexamportalexamportal".getBytes()); // Asegúrate de que la clave tenga al menos 256 bits
 
     public String extractUsername(String token) {
         return extractClaim(token, Claims::getSubject);
@@ -31,8 +35,9 @@ public class JwtUtils {
     }
 
     private Claims extractAllClaims(String token) {
-        return Jwts.parser()
-                .setSigningKey(SECRET_KEY)
+        return Jwts.parserBuilder() // Cambiado a parserBuilder()
+                .setSigningKey(SECRET_KEY) // Configuración de la clave secreta
+                .build() // Construcción del parser
                 .parseClaimsJws(token)
                 .getBody();
     }
@@ -52,7 +57,7 @@ public class JwtUtils {
                 .setSubject(subject)
                 .setIssuedAt(new Date(System.currentTimeMillis()))
                 .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 10)) // 10 horas
-                .signWith(SignatureAlgorithm.HS256, SECRET_KEY)
+                .signWith(SECRET_KEY, SignatureAlgorithm.HS256) // Usar la clave secreta aquí
                 .compact();
     }
 
